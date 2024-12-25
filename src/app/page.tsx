bde659441912afd2bf805a7b3230e8e1a228b784
egublog <egukei0729@gmail.com>
2024-12-25 23:43:07 +0900

    add: Command実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState<string>("");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [searchResults, setSearchResults] = useState<{
     results: string[];
     duration: number;
@@ -28,14 +35,47 @@ export default function Home() {
           A high-performance API built with Hono, Next.js and Cloudflare. <br />{" "}
           Type a query below and get your results in milliseconds.
         </p>
-        <input
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-          className="text-zinc-900"
-          type="text"
-        />
+
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results.length === 0 ? (
+                <CommandEmpty>No results found.</CommandEmpty>
+              ) : null}
+
+              {searchResults?.results ? (
+                <CommandGroup heading="Results">
+                  {searchResults?.results.map((result) => (
+                    <CommandItem
+                      key={result}
+                      value={result}
+                      onSelect={setInput}
+                    >
+                      {result}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              ) : null}
+
+              {searchResults?.results ? (
+                <>
+                  <div className="h-px w-full bg-zinc-200" />
+
+                  <p className="p-2 text-xs text-zinc-500">
+                    Found {searchResults.results.length} results in{" "}
+                    {searchResults?.duration.toFixed(0)}ms
+                  </p>
+                </>
+              ) : null}
+            </CommandList>
+          </Command>
+        </div>
       </div>
     </main>
   );

be469d96b7d2a23780360397d5d3c78f87849dd3
egublog <egukei0729@gmail.com>
2024-12-25 23:31:48 +0900

    add: SpeedSearchのタイトル

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,11 @@ export default function Home() {
   return (
     <main className="h-screen w-screen grainy">
       <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">SpeedSearch ⚡</h1>
+        <p className="text-zinc-600 text-lg max-w-prose text-center">
+          A high-performance API built with Hono, Next.js and Cloudflare. <br />{" "}
+          Type a query below and get your results in milliseconds.
+        </p>
         <input
           value={input}
           onChange={(e) => {
